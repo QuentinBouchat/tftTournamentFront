33ae554c618b2bda797ede3c13b87c28b1395791
Quentin Bouchat <quentinbouchat@hotmail.com>
2020-10-31 14:42:15 +0100

    Add loader

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <md-list>
+    <div class="loading" v-if="loading">
+      <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
+    </div>
+    <md-list v-else>
       <md-list-item v-for="tournament in tournaments" :key="tournament.id" :to="{ name: 'Tournament', params: { id: tournament.id } }">{{ tournament.name }}</md-list-item>
     </md-list>
   </div>
@@ -15,11 +18,20 @@ import { SimpleTournament } from '@/models';
   },
 })
 export default class HomePage extends Vue {
+  public loading = true;
   public tournaments: SimpleTournament[] = [];
 
   async mounted() {
     const tournaments = await this.$api.getTournaments();
     this.tournaments = tournaments;
+    this.loading = false;
   }
 }
 </script>
+
+<style lang="scss">
+.loading {
+  display: flex;
+  justify-content: center;
+}
+</style>
